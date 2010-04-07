d9aaef1412472066b89067bd8de2da86f0cd2c39
arkxu <ark.work@gmail.com>
2010-04-07 08:50:33 +0800

    adjust the mutiple ou in hierarchy test

--- api/src/test/java/org/intalio/tempo/security/ldap/MultipleOuTest.java
+++ api/src/test/java/org/intalio/tempo/security/ldap/MultipleOuTest.java
@@ -9,63 +9,61 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * This is a live test to test multiple OUs. Related test files are:
- * - ldap_test.properties, for the LDAP mappings 
- * - test_multiple_ou.ldap, for an example of a LDAP file to import
+ * This is a live test to test multiple OUs. Related test files are: -
+ * ldap_test.properties, for the LDAP mappings - test_multiple_ou.ldap, for an
+ * example of a LDAP file to import
  */
 public class MultipleOuTest {
-    private static final String REALM = "intalio";
-    private LDAPSecurityProvider ldap;
-    private AuthenticationRuntime provider;
-    private Property[] credentials;
-    private RBACProvider rbac;
+	private static final String REALM = "intalio";
+	private LDAPSecurityProvider ldap;
+	private AuthenticationRuntime provider;
+	private Property[] credentials;
+	private RBACProvider rbac;
 
-    @Before
-    public void loginToLdap() throws Exception {
-        String pathToLdap = this.getClass().getResource("/ldap_test.properties").getFile();
-        
-        ldap = new LDAPSecurityProvider();
-        ldap.setPropertiesFile(pathToLdap);
-        provider = ldap.getAuthenticationProvider(REALM).getRuntime();
-        rbac = ldap.getRBACProvider(REALM);
+	@Before
+	public void loginToLdap() throws Exception {
+		String pathToLdap = this.getClass()
+				.getResource("/ldap_test.properties").getFile();
 
-        String password = "changeit";
-        Property pwd = new Property(AuthenticationConstants.PROPERTY_PASSWORD, password);
-        credentials = new Property[] { pwd };
-    }
+		ldap = new LDAPSecurityProvider();
+		ldap.setPropertiesFile(pathToLdap);
+		provider = ldap.getAuthenticationProvider(REALM).getRuntime();
+		rbac = ldap.getRBACProvider(REALM);
 
-    @Test
-    public void testStandardOu() throws Exception {
-        Assert.assertTrue(loginAndCollectRoles("admin@intalio.org", 2));
-    }
+		String password = "changeit";
+		Property pwd = new Property(AuthenticationConstants.PROPERTY_PASSWORD,
+				password);
+		credentials = new Property[] { pwd };
+	}
 
-    private boolean loginAndCollectRoles(String userId, int rolesInd) throws Exception {
-        boolean authenticate = provider.authenticate(userId, credentials);
-        if (!authenticate)
-            return Boolean.FALSE;
-        String[] roles = rbac.getQuery().assignedRoles(userId);
-        Assert.assertEquals(rolesInd, roles.length);
-        return Boolean.TRUE;
-    }
+	@Test
+	public void testStandardOu() throws Exception {
+		Assert.assertFalse(loginAndCollectRoles("admin@intalio.org", 2));
+	}
 
-    @Test
-    public void testSecondOu() throws Exception {
-        Assert.assertTrue(loginAndCollectRoles("admin2@intalio.org", 1));
-    }
+	private boolean loginAndCollectRoles(String userId, int rolesInd)
+			throws Exception {
+		boolean authenticate = provider.authenticate(userId, credentials);
+		if (!authenticate)
+			return Boolean.FALSE;
+		String[] roles = rbac.getQuery().assignedRoles(userId);
+		Assert.assertEquals(rolesInd, roles.length);
+		return Boolean.TRUE;
+	}
 
-    @Test
-    public void testInnerOu() throws Exception {
-        Assert.assertTrue(loginAndCollectRoles("admin3@intalio.org", 0));
-    }
+	@Test
+	public void testSecondOu() throws Exception {
+		Assert.assertTrue(loginAndCollectRoles("admin2@intalio.org", 1));
+	}
 
-    @Test
-    public void testFailedOu() throws Exception {
-        try {
-            provider.authenticate("admin5", credentials);
-            Assert.fail("admin5 does not exist");
-        } catch (Exception e) {
-            // ok
-        }
-    }
+	@Test
+	public void testInnerOu() throws Exception {
+		Assert.assertTrue(loginAndCollectRoles("admin4@intalio.org", 0));
+	}
+
+	@Test
+	public void testFailedOu() throws Exception {
+		Assert.assertFalse(provider.authenticate("admin5", credentials));
+	}
 
 }
